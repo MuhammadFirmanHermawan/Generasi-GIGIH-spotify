7cb62fefcdbfc107804f8e2aeda171c1255ffd4a
MuhammadFirmanHermawan <muhammadfirmanhermawan0607@gmail.com>
2022-03-20 15:54:56 +0700

    Spotify API (masih ada error)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        spotify_main: "#1ED760",
+        dark_main: "#0E0E0E",
+        spotify_card: "#2B2B2B",
+        spotify_card_hover: "#383838",
+      },
+    },
+  },
+  plugins: [],
+};
